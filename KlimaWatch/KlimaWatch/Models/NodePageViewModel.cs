545e7ca98c7f32a5a6236d680d93443b9e4c63fe
TvTTiT <thanhtvfs0830@gmail.com>
2023-01-14 22:34:16 +0100

    update

---
+++ KlimaWatch/KlimaWatch/Models/NodePageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using KlimaWatch.Data.Entities.Nodes;
+
+namespace KlimaWatch.Models;
+
+public class NodePageViewModel
+{
+    public NodeInfo NodeInfo { get; set; }
+    public ICollection<NodeMessage> NodeMessages { get; set; }
+}
